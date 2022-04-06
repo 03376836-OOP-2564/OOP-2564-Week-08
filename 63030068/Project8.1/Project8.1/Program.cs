f5b65e8da4d59ec47415d556e0b44ec1522de8c0
xNewz <pargornruasijan@gmail.com>
2022-04-06 14:39:16 +0700

    Add 63030068 Pargorn (Final)

---
+++ 63030068/Project8.1/Project8.1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project8._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int var = 30;
+            int var1;
+            int var2, var3;
+            int var4 = var5;
+            int var6 = 2, var7;
+            int var8 = 10 * 5;
+            int var9 = var;
+            int var10, c1, f1;
+            double d1 = false;
+            bool b1 = 0;
+        }
+    }
+}
