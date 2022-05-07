abb300faf3bb1054729f2313f225eabad8b74c72
Piyachat <63030052@kmitl.ac.th>
2022-05-08 00:46:44 +0700

    add set 4 method main

--- 63030052/Project 8.2/Project 8.2/Program.cs
+++ 63030052/Project 8.2/Project 8.2/Program.cs
@@ -10,19 +10,17 @@ namespace Project_8._2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine($"         1111111111222222");
-            Console.WriteLine($"1234567890123456789012345");
-            Console.WriteLine($"{1,0}");
-            Console.WriteLine($"{1,1}");
-            Console.WriteLine($"{1,2}");
-            Console.WriteLine($"{1,3}");
-            Console.WriteLine($"{1,4}");
-            Console.WriteLine($"{1,5}");
-            Console.WriteLine($"{1,10}");
-            Console.WriteLine($"{1,15}");
-            Console.WriteLine($"{1,20}");
-            Console.WriteLine($"{1,22}");
-            Console.WriteLine($"{1,25}");
+            int i = 123456789;
+            Console.WriteLine("Regular string format");
+            Console.WriteLine("{0}", i);
+            Console.WriteLine("String interpreter");
+            Console.WriteLine($"None ==> {i}");
+            Console.WriteLine($"   E ==> {i:E}");
+            Console.WriteLine($"   F ==> {i:F}");
+            Console.WriteLine($"   G ==> {i:G}");
+            Console.WriteLine($"   N ==> {i:N}");
+            Console.WriteLine($"   P ==> {i:P}");
+            Console.WriteLine($"   X ==> {i:X}");
         }
     }
 }
