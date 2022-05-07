b594c79570c662ef568573ab367afda5a4e0abb9
Piyachat <63030052@kmitl.ac.th>
2022-05-08 00:45:49 +0700

    add set 3 method main

--- 63030052/Project 8.2/Project 8.2/Program.cs
+++ 63030052/Project 8.2/Project 8.2/Program.cs
@@ -10,11 +10,19 @@ namespace Project_8._2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine($"{3} and {1}");
-            Console.WriteLine($"{3} and {1}");
-            Console.WriteLine($"{3.0d} and {1.0001d}");
-            Console.WriteLine($"{3:F2} and {1000.123:F1}");
-            Console.WriteLine($"{3.123456:F2} and {5.123000:F4}");
+            Console.WriteLine($"         1111111111222222");
+            Console.WriteLine($"1234567890123456789012345");
+            Console.WriteLine($"{1,0}");
+            Console.WriteLine($"{1,1}");
+            Console.WriteLine($"{1,2}");
+            Console.WriteLine($"{1,3}");
+            Console.WriteLine($"{1,4}");
+            Console.WriteLine($"{1,5}");
+            Console.WriteLine($"{1,10}");
+            Console.WriteLine($"{1,15}");
+            Console.WriteLine($"{1,20}");
+            Console.WriteLine($"{1,22}");
+            Console.WriteLine($"{1,25}");
         }
     }
 }
