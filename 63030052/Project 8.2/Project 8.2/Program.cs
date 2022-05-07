afe53277ab49f5c662d98423f861095df9e5ad50
Piyachat <63030052@kmitl.ac.th>
2022-05-08 00:43:22 +0700

    add set 1 method main

--- 63030052/Project 8.2/Project 8.2/Program.cs
+++ 63030052/Project 8.2/Project 8.2/Program.cs
@@ -10,6 +10,11 @@ namespace Project_8._2
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("{0} and {1}", 3, 5);
+            Console.WriteLine("{0} and {1}", 3.0, 5.0);
+            Console.WriteLine("{0} and {1}", 3.0d, 5.0d);
+            Console.WriteLine("{0:F1} and {1:F1}", 3.0, 5.0);
+            Console.WriteLine("{0:F2} and {1:F2}", 3.0d, 5.0d);
         }
     }
 }
