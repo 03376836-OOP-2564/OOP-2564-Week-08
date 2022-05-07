f22c484292e3a7c8d8af22a816f54bc55be57efb
Piyachat <63030052@kmitl.ac.th>
2022-05-08 03:37:40 +0700

    add code

--- 63030052/Project 8.3/Project 8.3/Program.cs
+++ 63030052/Project 8.3/Project 8.3/Program.cs
@@ -4,12 +4,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Project_8._3
+
+namespace method_examples
 {
+    class number
+    {
+        static public int numberInt1;
+        static public double numberDouble1;
+        public int numberInt2;
+        public double numberDouble2;
+    }
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            number.numberInt1 = 10;
+            number.numberInt2 = 20;
+            number.numberDouble1 = 100.500;
+            number.numberDouble2 = 200.500;
+
+            Console.WriteLine($"numberInt1 = {number.numberInt1}");
+            Console.WriteLine($"numberInt2 = {number.numberInt2}");
+            Console.WriteLine($"numberDouble1 = {number.numberDouble1}");
+            Console.WriteLine($"numberDouble2 = {number.numberDouble2}");
+
         }
     }
 }
