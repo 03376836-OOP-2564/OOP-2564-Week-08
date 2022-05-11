c0d3a33620c2605dcbe028344612b9ca20138060
Napasin_S <63030041@kmitl.ac.th>
2022-05-11 20:19:37 +0700

    bruh2

---
+++ 63030041/Project 8.1/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int var = 30;
+            int var1;
+            int var2, var3;
+            int var4 = var5;
+            int var6 = 2, var7;
+            int var8 = 10 * 5;
+            int var9 = var;
+            int var10, char c1, float f1;
+            double d1 = false;
+            bool b1 = 0;
+        }
+    }
+}
