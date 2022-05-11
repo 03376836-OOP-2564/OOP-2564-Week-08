6dc6929bb96ad1ee72db2b19f72e90b4f0be76a3
63030088sarunchana <63030088@kmitl.ac.th>
2022-05-11 22:03:47 +0700

    8.2

---
+++ 63030088/Project8.2/Project8.2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Project8._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("{0} and {1}", 3, 5);
+            Console.WriteLine("{0} and {1}", 3.0, 5.0);
+            Console.WriteLine("{0} and {1}", 3.0d, 5.0d);
+            Console.WriteLine("{0:F1} and {1:F1}", 3.0, 5.0);
+            Console.WriteLine("{0:F2} and {1:F2}", 3.0d, 5.0d);
+        }
+    }
+}
