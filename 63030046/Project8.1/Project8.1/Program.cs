6fe363d231c89a9b0c4deef4a5989b7c086e75eb
bancha96 <63030046@kmitl.ac.th>
2022-05-11 22:08:47 +0700

    Project8.2-8.3

--- 63030046/Project8.1/Project8.1/Program.cs
+++ 63030046/Project8.1/Project8.1/Program.cs
@@ -6,7 +6,16 @@ namespace Project8._1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int var = 30;
+            int var1;
+            int var2, var3;
+            int var4 = var5;
+            int var6 = 2, var7;
+            int var8 = 10 * 5;
+            int var9 = var;
+            int var10, char c1, float f1;
+            double d1 = false;
+            bool b1 = 0;
         }
     }
 }
