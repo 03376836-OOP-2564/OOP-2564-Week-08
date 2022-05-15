e456fd55aee2c5725e0abcdec3759483e8e55904
Pramotjonsungnoen <92082350+Pramotjonsungnoen@users.noreply.github.com>
2022-05-16 02:59:42 +0700

    63030050

--- 63030050/project 8.3/Program.cs
+++ 63030050/project 8.3/Program.cs
@@ -1,19 +1,33 @@
-﻿string name = "Hello";
-Console.WriteLine(String.Format("{0} there. I said {0}! {0}???", name));
-Console.WriteLine($"{2:d} {0:d} {1:d}", 1, 2, 3);
-Console.WriteLine($"Hello " + $"World");
-Console.WriteLine($"Here comes a slash \\");
-Console.WriteLine($"|{999,10}|");
-Console.WriteLine($"|{000,-10}|");
-Console.WriteLine($"The value: {500}.");
-Console.WriteLine($"The value: {500:C}.");
-Console.WriteLine($"{12.3456789,-10:F4}");
-Console.WriteLine($"{12.3456789,-10:C}");
-Console.WriteLine($"{12.3456789,-10:E3}");
-Console.WriteLine($"{65535,-10:x}");
-Console.WriteLine($"{65535,-10:X}");
-int i;
-Console.WriteLine("Value\tSquared\tCubed");
-for (i = 1; i < 10; i++)
-    Console.WriteLine($"{i}\t{i * i}\t{i * i * i}");
-Console.WriteLine($"{1234.56789:#.###}.");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace method_examples
+{
+    class number
+    {
+        static public int numberInt1;
+        static public double numberDouble1;
+        public int numberInt2;
+        public double numberDouble2;
+    }
+    class Program
+    {
+        static void Main()
+        {
+            number.numberInt1 = 10;
+            number.numberInt2 = 20;
+            number.numberDouble1 = 100.500;
+            number.numberDouble2 = 200.500;
+
+            Console.WriteLine($"numberInt1 = {number.numberInt1}");
+            Console.WriteLine($"numberInt2 = {number.numberInt2}");
+            Console.WriteLine($"numberDouble1 = {number.numberDouble1}");
+            Console.WriteLine($"numberDouble2 = {number.numberDouble2}");
+
+        }
+    }
+}
