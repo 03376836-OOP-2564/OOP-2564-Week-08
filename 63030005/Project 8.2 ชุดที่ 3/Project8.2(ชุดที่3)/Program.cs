991e4600cb68ab9f3439e7df6af22cfa225900bb
AomKusuma005 <63030005@kmitl.ac.th>
2022-05-21 21:07:38 +0700

    Add By 63030005

---
+++ 63030005/Project 8.2 ชุดที่ 3/Project8.2(ชุดที่3)/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace project__8._2_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"         1111111111222222");
+            Console.WriteLine($"1234567890123456789012345");
+            Console.WriteLine($"{1,0}");
+            Console.WriteLine($"{1,1}");
+            Console.WriteLine($"{1,2}");
+            Console.WriteLine($"{1,3}");
+            Console.WriteLine($"{1,4}");
+            Console.WriteLine($"{1,5}");
+            Console.WriteLine($"{1,10}");
+            Console.WriteLine($"{1,15}");
+            Console.WriteLine($"{1,20}");
+            Console.WriteLine($"{1,22}");
+            Console.WriteLine($"{1,25}");
+        }
+    }
+}
