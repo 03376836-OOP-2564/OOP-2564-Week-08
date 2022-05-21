b6e3e4c50c3bd1f39c0b2fdc0f014d22282c1c89
Atpolink <92082676+Atpolink@users.noreply.github.com>
2022-05-21 22:46:30 +0700

    เพิ่มโค้ด

--- 63030023/8.2/8.2/8.2/Program.cs
+++ 63030023/8.2/8.2/8.2/Program.cs
@@ -1,2 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+int a = 100;
+string s = $"a = {a}";
+
+string name = "Hello";
+Console.WriteLine(String.Format("{0} there. I said {0}! {0}???", name));
+Console.WriteLine($"{2:d} {0:d} {1:d}", 1, 2, 3);
+Console.WriteLine($"Hello " + $"World");
+Console.WriteLine($"Here comes a slash \\");
+Console.WriteLine($"|{999,10}|");
+Console.WriteLine($"|{000,-10}|");
+Console.WriteLine($"The value: {500}.");
+Console.WriteLine($"The value: {500:C}.");
+Console.WriteLine($"{12.3456789,-10:F4}");
+Console.WriteLine($"{12.3456789,-10:C}");
+Console.WriteLine($"{12.3456789,-10:E3}");
+Console.WriteLine($"{65535,-10:x}");
+Console.WriteLine($"{65535,-10:X}");
+int i;
+Console.WriteLine("Value\tSquared\tCubed");
+for (i = 1; i < 10; i++)
+    Console.WriteLine($"{i}\t{i * i}\t{i * i * i}");
+Console.WriteLine($"{1234.56789:#.###}.");
