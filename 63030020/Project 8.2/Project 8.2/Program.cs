414734d70292ed0611da521d7a10486736d7ce30
Chutipon01 <arm9423@gmail.com>
2022-05-21 18:31:08 +0700

    add Project 8.2

---
+++ 63030020/Project 8.2/Project 8.2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_8._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("{0} and {1}", 3, 5);
+            Console.WriteLine("{0} and {1}", 3.0, 5.0);
+            Console.WriteLine("{0} and {1}", 3.0d, 5.0d);
+            Console.WriteLine("{0:F1} and {1:F1}", 3.0, 5.0);
+            Console.WriteLine("{0:F2} and {1:F2}", 3.0d, 5.0d);
+        }
+    }
+}
